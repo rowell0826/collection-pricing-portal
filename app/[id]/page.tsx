5ed543b8f1178d538f65d9d6a065e344ea0ae368
arvee0220 <rowellverdejo@gmail.com>
2024-11-15 02:33:00 +0800

    Refactor code to properly fetched passed props

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,118 +1,12 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { db } from "@/lib/firebase/firebase";
-import { ArtWork } from "@/lib/types/artworkTypes";
-import { doc, getDoc } from "firebase/firestore";
-import { ArrowLeft } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import ViewArt from "@/components/viewArt/ViewArt";
 
-interface ViewArtProps {
+interface PageProps {
 	params: { id: string };
 }
 
-const ViewArt: React.FC<ViewArtProps> = ({ params }) => {
-	const [artwork, setArtwork] = useState<ArtWork | null>(null);
-	const [error, setError] = useState<string | null>(null);
-	const [loading, setLoading] = useState<boolean>(true);
-
+export default function Page({ params }: PageProps) {
 	const { id } = params;
+	console.log("Route param ID:", id);
 
-	useEffect(() => {
-		const fetchArtwork = async () => {
-			try {
-				const artRef = doc(db, "artworks", id);
-				const artSnapshot = await getDoc(artRef);
-
-				if (artSnapshot.exists()) {
-					setArtwork(artSnapshot.data() as ArtWork);
-				} else {
-					setError("Artwork not found.");
-					console.warn("Document does not exist for ID:", id);
-				}
-			} catch (error) {
-				setError("Failed to load artwork data.");
-				console.error("Error fetching data:", error);
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		fetchArtwork();
-	}, [id]);
-
-	if (loading) return <div>Loading...</div>;
-	if (error) return <div>{error}</div>;
-
-	const {
-		title,
-		artist_full_name,
-		artist_birth,
-		description,
-		provenance,
-		sale_price,
-		medium,
-		dimensions: { length, width, height },
-		area,
-		aspect_ratio,
-	} = artwork as ArtWork;
-
-	return (
-		<section className="w-screen h-full md:h-screen flex flex-col md:flex-row bg-gray-100 overflow-y-scroll scrollbar-hide">
-			{/* Image Area */}
-			<div className="relative md:flex-1 h-screen w-screen md:w-[80%] bg-none">
-				<Link href={"/"} className="absolute z-10">
-					<Button className="ml-4 mt-4" size={"sm"}>
-						<ArrowLeft />
-					</Button>
-				</Link>
-				<div className="relative h-screen w-screen bg-gray-200">
-					{artwork?.img_url.map((img, index) => (
-						<Image
-							key={index}
-							src={img}
-							alt="Artwork image"
-							fill
-							className="object-cover shadow-lg"
-						/>
-					))}
-				</div>
-			</div>
-
-			{/* Side Dashboard */}
-			<Card className="relative md:h-screen md:w-[20%] shadow-lg p-6">
-				<CardHeader>
-					<CardTitle className="font-semibold text-md">{title}</CardTitle>
-				</CardHeader>
-				<CardContent className="flex flex-col gap-4">
-					<h4 className="text-md font-medium">
-						{artist_full_name}
-						<span className="text-gray-500 text-sm">
-							{Number.isNaN(artist_birth) ? "" : `, ${artist_birth}`}
-						</span>
-					</h4>
-					<p className="text-sm">{description}</p>
-					{provenance && <p className="text-sm font-light">Provenance: {provenance}</p>}
-					{medium && <p className="text-sm font-light">Medium: {medium}</p>}
-					<div className="space-y-1 flex flex-col items-end">
-						<p className="text-sm font-light self-start">Dimensions:</p>
-						{Number.isNaN(length) ? "" : <p className="text-sm">Length: {length} cm</p>}
-						<div className="flex flex-col items-start">
-							<p className="text-sm">Width: {width} cm</p>
-							<p className="text-sm">Height: {height} cm</p>
-							<p className="text-sm">Area: {area} cm²</p>
-							<p className="text-sm">Aspect Ratio: {aspect_ratio}</p>
-						</div>
-					</div>
-				</CardContent>
-				<CardFooter className="text-xs text-foreground font-medium">
-					Sale price: {sale_price} USD
-				</CardFooter>
-			</Card>
-		</section>
-	);
-};
-
-export default ViewArt;
+	return <ViewArt params={{ id }} />;
+}
