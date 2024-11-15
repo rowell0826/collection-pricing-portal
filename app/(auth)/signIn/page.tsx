a9629e8ba62c985fd2f405b26108663f1b4713f3
arvee0220 <rowellverdejo@gmail.com>
2024-11-16 04:55:28 +0800

    Modify barker logo for dark theme

--- app/(auth)/signIn/page.tsx
+++ app/(auth)/signIn/page.tsx
@@ -1,13 +1,19 @@
-import { barkerLogo } from "@/components/constants/constants";
+import { barkerLogo, barkerLogoDk } from "@/components/constants/constants";
 import SignInForm from "@/components/signInForm/SignInForm";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import React from "react";
 
 const SignIn = () => {
+	const { theme } = useTheme();
 	return (
 		<section className="w-full h-screen flex flex-col justify-center items-center my-auto">
 			<div className="w-40 h-20 flex justify-center items-center">
-				<Image src={barkerLogo} alt="Barker Logo" className="pr-4 object-fill" />
+				<Image
+					src={theme === "light" ? barkerLogo : barkerLogoDk}
+					alt="Barker Logo"
+					className="pr-4 object-fill"
+				/>
 			</div>
 			<SignInForm />
 		</section>
