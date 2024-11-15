2e80f4277aa5115dcc89d12a0a08277ac672f2af
arvee0220 <rowellverdejo@gmail.com>
2024-11-16 04:59:20 +0800

    Add theme context

--- app/(auth)/signIn/page.tsx
+++ app/(auth)/signIn/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { barkerLogo, barkerLogoDk } from "@/components/constants/constants";
 import SignInForm from "@/components/signInForm/SignInForm";
 import { useTheme } from "next-themes";
